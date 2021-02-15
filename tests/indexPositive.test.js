5f9f91473506eb0ba05a67f1dd11318b398c8b55
Vladislav Krysin <krysin18@mail.ru>
2021-02-15 07:04:06 +0300

    test: fix smell usless variable roots in Positive tests

--- tests/indexPositive.test.js
+++ tests/indexPositive.test.js
@@ -11,8 +11,6 @@ describe('(1,5,4)={-1,-4}', () => {
   document.getElementsByClassName("bIndex")[0].value = 5
   document.getElementsByClassName("cIndex")[0].value = 4
   test('find right roots', () => {
-    const roots = findRoots(1, 5, 4)
-    
     expect(findRoots(1,5,4)).toEqual({
       x1: -1,
       x2: -4
